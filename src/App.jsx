2f7ab40772f7dbeec56473d615f19f91cc6c601b
inarey <thoselovelywords@gmail.com>
2025-01-15 16:11:51 +0900

    New Project with Wesley

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+    <div className="bg-zinc-900 h-screen flex justify-center items-center">
+      <h1>Word Analytics</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
