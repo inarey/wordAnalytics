ec24860dd40a59fa98aade30108c6708f51c06da
inarey <thoselovelywords@gmail.com>
2025-01-15 16:24:30 +0900

    Added fonts and styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
     <div className="bg-zinc-900 h-screen flex justify-center items-center">
-      <h1>Word Analytics</h1>
+      <h1 className="text-zinc-50 text-4xl">Word Analytics</h1>
     </div>
     </>
   )
