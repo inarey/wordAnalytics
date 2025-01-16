53034a41da434693e16fdac57f17c2d4110aa0a4
inarey <thoselovelywords@gmail.com>
2025-01-16 17:36:03 +0900

    worked on the numbers and character status

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,12 +1,21 @@
-import Stats from "./Stats";
-import Textarea from "./Textarea";
+import { useState } from 'react';
+import Stats from './Stats';
+import Textarea from './Textarea';
 
 export default function Content() {
+  const [text, setText] = useState('');
+
+  const stats = {
+    numberOfWords: text.split(' ').filter((word) => word !== '').length,
+    numberOfCharacters: text.length,
+    numberOfIgCharacters: 280 - text.length,
+    numberOfFbCharacters: 2200 - text.length,
+  };
   return (
     <>
-      <main className="container">
-        <Textarea />
-        <Stats />
+      <main className='container'>
+        <Textarea text={text} setText={setText} />
+        <Stats stats={stats} />
       </main>
     </>
   );
--- src/components/Textarea.jsx
+++ src/components/Textarea.jsx
@@ -1,36 +1,37 @@
 import { useState } from 'react';
 import Warning from './Warning';
 
-export default function Textarea() {
-  const [text, setText] = useState('');
+export default function Textarea({ text, setText }) {
   // const [showWarning, setShowWarning] = useState(false);
   const [warningText, setWarningText] = useState('');
 
   const handleChange = (e) => {
-      let newText = e.target.value;
+    let newText = e.target.value;
 
-      if (newText.includes('<script>')) {
-        setWarningText("No script tag allowed!");
-        // setShowWarning(true);
-        newText = newText.replace('<script>', '');
-      }else if (newText.includes('@')) {
-        setWarningText("No @ symbol allowed!");
-        // setShowWarning(true);
-        newText = newText.replace('@', '');
-      }
-        setText(newText);
+    if (newText.includes('<script>')) {
+      setWarningText('No script tag allowed!');
+      // setShowWarning(true);
+      newText = newText.replace('<script>', '');
+    } else if (newText.includes('@')) {
+      setWarningText('No @ symbol allowed!');
+      // setShowWarning(true);
+      newText = newText.replace('@', '');
+    } else {
+      setWarningText('');
+    }
+    setText(newText);
   };
   return (
     <>
-    <div className='textarea'>
-      <textarea
-        value={text}
-        onChange={handleChange}
-        placeholder='type your text here...'
-        spellCheck='false'
-      />
-      {/* {warningText && <Warning warningText={warningText}/>} */}
-      {<Warning warningText={warningText}/>}
+      <div className='textarea'>
+        <textarea
+          value={text}
+          onChange={handleChange}
+          placeholder='type your text here...'
+          spellCheck='false'
+        />
+        {/* {warningText && <Warning warningText={warningText}/>} */}
+        {<Warning warningText={warningText} />}
       </div>
     </>
   );
