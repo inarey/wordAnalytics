1bc752aa98405908397ce8950a989ca9ba2511f4
inarey <thoselovelywords@gmail.com>
2025-01-20 20:19:01 +0900

    Bold text on the footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <>
       <footer className="footer">
-        <small>&copy; ByteGrad 2025 recoded by Shan-Ti</small>
+        <small>&copy; ByteGrad 2025 recoded by <b>Shan-Ti</b></small>
         <small>Last checked limits 17 days ago</small>
       </footer>
     </>
